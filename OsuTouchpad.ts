4067a95e5f47fa4ab635aea0739b8ca5a26b68fc
Ashleyz4 <epicman56@yahoo.com>
2019-07-03 15:33:52 -0500

    update 1

--- OsuTouchpad.ts
+++ OsuTouchpad.ts
@@ -1,46 +1,42 @@
-var express = require('express');
+import * as express from 'express';
+import * as http from 'http';
+import * as ip from 'ip'
+var parser = require('ua-parser-js'); // no way to change this(?)
+import * as path from 'path';
+import * as robotjs from 'robotjs'
+import * as bodyParser from 'body-parser';
 var app = express();
-var html = require('http').Server(app);
-var ip = require('ip');
-var parser = require('ua-parser-js');
-var path = require('path');
-var robot = require("robotjs");
-
-var bodyParser = require('body-parser');
+var html = new http.Server(app);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "/public/")));
+app.use(express.static(path.join(__dirname, '/public/')));
 
-var pop = () => {
-    console.log();
-    console.log();
-};
 app.get('/', (req, res) => {
-    pop();
+    console.log('\n\n'); 
     var ua_data = parser(req.headers['user-agent']);
-	var OS_NAME = ua_data.os.name;
-	console.log("OS: " + OS_NAME);
-	var MODEL_NAME = ua_data.device.model;
-    console.log("Model: " + MODEL_NAME);
-    
-    res.sendFile(path.join(__dirname + "/index.html"));
+    var OS_NAME = ua_data.os.name;
+    console.log('OS: ' + OS_NAME);
+    var MODEL_NAME = ua_data.device.model;
+    console.log('Model: ' + MODEL_NAME);
+
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.post("/console", (req, res) => {
+app.post('/console', (req, res) => {
     var value = req.query.value;
     res.end();
 });
-app.post("/click", (req, res) => {
-    robot.mouseClick();
+app.post('/click', (req, res) => {
+    robotjs.mouseClick();
     res.end();
 });
-app.post("/hi", (req, res) => {
-    console.log("CONNECTED");
+app.post('/hi', (req, res) => {
+    console.log('CONNECTED');
     res.end();
 });
-var screen_size = robot.getScreenSize();
-app.get("/getXY", (req, res) => {
+var screen_size = robotjs.getScreenSize();
+app.get('/getXY', (req, res) => {
     var x = req.query.x;
     var y = req.query.y;
     var width = req.query.width;
@@ -51,16 +47,11 @@ app.get("/getXY", (req, res) => {
     var h_ratio = (sH / height);
     var moveX = x * w_ratio;
     var moveY = y * h_ratio;
-    robot.moveMouse(moveX, moveY);
+    robotjs.moveMouse(moveX, moveY);
     res.end();
 });
 
-var PORT = 3000;
-try {
-    html.listen(3000, "0.0.0.0", () => {
-        var server_ip = ip.address() + ":" + 3000
-        console.log("IP to join: " + server_ip);
-    });
-} catch(e) {
-
-}
+html.listen(3000, '0.0.0.0', () => {
+    var server_ip = ip.address() + ':' + 3000
+    console.log('IP to join: ' + server_ip);
+});
